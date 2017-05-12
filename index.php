9c7b3a0865fd9ae46ff6d7fd0e3f608e6bcaac0d
Jamie Harrell <jharrell@gmail.com>
2017-05-12 12:47:21 -0300

    Move to below-the-fold

--- index.php
+++ index.php
@@ -49,9 +49,6 @@ function displayLatestPictures($num) {
     <meta charset="utf-8" />
     <title>Harrell.CA</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300|Montserrat:400,700' rel='stylesheet' type='text/css' async />
-    <link href='css/font-awesome.min.css' rel='stylesheet' type='text/css' />
-    <link href='style.css' rel='stylesheet' type='text/css' />
   </head>
   <body>
     <div class="sidebar">
@@ -92,5 +89,23 @@ function displayLatestPictures($num) {
         </div>
       </div>
     </div>
+    <noscript id="deferred-styles">
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300|Montserrat:400,700' rel='stylesheet' type='text/css' async />
+      <link href='css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+      <link href='css/style.css' rel='stylesheet' type='text/css' />
+    </noscript>
+    <script>
+      var loadDeferredStyles = function() {
+        var addStylesNode = document.getElementById("deferred-styles");
+        var replacement = document.createElement("div");
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement)
+        addStylesNode.parentElement.removeChild(addStylesNode);
+      };
+      var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+          webkitRequestAnimationFrame || msRequestAnimationFrame;
+      if (raf) raf(function() { window.setTimeout(loadDeferredStyles, 0); });
+      else window.addEventListener('load', loadDeferredStyles);
+    </script>
   </body>
 </html>
